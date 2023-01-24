4e707e747c5c9308562e8c760acf6e07c64a4ae0
mdthr_ah <modather11.a@gmail.com>
2023-01-24 23:56:36 +0200

    update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,11 +1,11 @@
-##include "main.h"
+#include "main.h"
 
 void print_buffer(char buffer[], int *buff_ind);
 
 /**
  * _printf - Printf function
- * @format: format
- * Return: Printed chars
+ * @format: format.
+ * Return: Printed chars.
  */
 int _printf(const char *format, ...)
 {
@@ -26,6 +26,7 @@ int _printf(const char *format, ...)
 			buffer[buff_ind++] = format[i];
 			if (buff_ind == BUFF_SIZE)
 				print_buffer(buffer, &buff_ind);
+			/* write(1, &format[i], 1);*/
 			printed_chars++;
 		}
 		else
@@ -37,30 +38,29 @@ int _printf(const char *format, ...)
 			size = get_size(format, &i);
 			++i;
 			printed = handle_print(format, &i, list, buffer,
-					flags, width, precision, size);
+				flags, width, precision, size);
 			if (printed == -1)
 				return (-1);
 			printed_chars += printed;
 		}
-
-		print_buffer(buffer, &buff_ind);
-
-		va_end(list);
-
-		return (printed_chars);
 	}
 
+	print_buffer(buffer, &buff_ind);
 
-	/**
-	 * print_buffer - Prints the content of buffer
-	 * @buffer: Array of chars
-	 * @buff_ind: Index at which to add next char, represents the length
-	 */
+	va_end(list);
 
-	void print_buffer(char buffer[], int *buff_ind)
-	{
-		if (*buff_ind > 0)
-			write(1, &buffer[0], *buff_ind);
+	return (printed_chars);
+}
 
-		*buff_ind = 0;
-	}
+/**
+ * print_buffer - Prints the contents of the buffer if it exist
+ * @buffer: Array of chars
+ * @buff_ind: Index at which to add next char, represents the length.
+ */
+void print_buffer(char buffer[], int *buff_ind)
+{
+	if (*buff_ind > 0)
+		write(1, &buffer[0], *buff_ind);
+
+	*buff_ind = 0;
+}
